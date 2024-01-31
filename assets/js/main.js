73e23a39a349da5a784f9f0a8c7c0b5b3a42597d
Công Thoại <dcthoai1023@gmail.com>
2024-01-31 22:51:10 +0700

    add css responsive for mobile

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,27 @@
 var moveUpButton = document.querySelector('.move-up');
+var navbarButton = document.getElementById('navbar');
+var navlist = document.querySelector('.header .navlist');
 
-console.log(moveUpButton)
+navbarButton.addEventListener('mousedown', function() {
+    navbarButton.classList.add('active');
+});
+
+navbarButton.addEventListener('mouseup', function() {
+    if (navbarButton.classList.contains('active')){
+        navbarButton.classList.remove('active');
+    }
+
+    if (navlist.classList.contains('active')){
+        setTimeout(function(){
+            navlist.classList.remove('active');
+            navbarButton.innerHTML = '<i class="fa-solid fa-bars"></i>';
+        }, 300);
+    } else {
+        navlist.classList.add('active');     
+        navbarButton.innerHTML = '<i class="fa-solid fa-xmark"></i>';
+    }
+});
+
 
 window.addEventListener("scroll", function(){
     if (window.scrollY > 100){
